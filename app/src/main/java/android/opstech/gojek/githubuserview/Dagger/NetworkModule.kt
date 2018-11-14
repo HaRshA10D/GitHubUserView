b53ef8116ad53c9db242c1d4ceae2a5239cf2a32
harsha.hd <harsha.hd@go-jek.com>
2018-11-14 17:13:56 +0530

    [HarshaD] Added network module, gives network objects

---
+++ app/src/main/java/android/opstech/gojek/githubuserview/Dagger/NetworkModule.kt
@@ -0,0 +1,39 @@
+package android.opstech.gojek.githubuserview.Dagger
+
+import android.opstech.gojek.githubuserview.RetrofitApiPaths.GitHubService
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+class NetworkModule(val baseUrl: String) {
+
+    @Provides
+    fun providesOkHttpClient(): OkHttpClient {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val builder = OkHttpClient.Builder()
+        builder.addInterceptor(httpLoggingInterceptor)
+        return builder.build()
+    }
+
+    @Provides
+    fun providesRetrofit(client: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+    }
+
+    @Provides
+    fun providesGitHubService(retrofit: Retrofit): GitHubService {
+        return retrofit.create(GitHubService::class.java)
+    }
+}
